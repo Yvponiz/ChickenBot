2d640c94c6f64205f8eac42e5034e65ebca038c4
Yvponiz <yvanoski_s@hotmail.com>
2022-06-16 18:09:21 -0400

    update gitignore

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,7 +2,7 @@ import { Client, ClientOptions } from "discord.js";
 import interactionCreate from "./listeners/interactionCreate";
 import ready from "./listeners/ready";
 
-const token = "OTg3MDcwMTM4NzE1MDc4NjY2.GnhEy0.5agxSrk-KyHR1dHgJk5dX_rnT63Gt1IfKlM4hE"
+const token = "OTg3MDcwMTM4NzE1MDc4NjY2.GnhEy0.5agxSrk-KyHR1dHgJk5dX_rnT63Gt1IfKlM4hE" //generate new asap
 
 console.log("Bot is starting...");
 
