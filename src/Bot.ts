ddb54a37a4841e7ab0692c29bd01e8fac6a16e26
Yvponiz <yvanoski_s@hotmail.com>
2022-06-16 18:04:35 -0400

    first commit

---
+++ src/Bot.ts
@@ -0,0 +1,16 @@
+import { Client, ClientOptions } from "discord.js";
+import interactionCreate from "./listeners/interactionCreate";
+import ready from "./listeners/ready";
+
+const token = "OTg3MDcwMTM4NzE1MDc4NjY2.GnhEy0.5agxSrk-KyHR1dHgJk5dX_rnT63Gt1IfKlM4hE"
+
+console.log("Bot is starting...");
+
+const client = new Client({
+    intents: []
+});
+
+ready(client);
+interactionCreate(client);
+
+client.login(token);
---
+++ src/Commands.ts
@@ -0,0 +1,5 @@
+import { Command } from "./Command";
+import { Gab } from "./commands/Gab";
+import { Hello } from "./commands/Hello";
+
+export const Commands: Command[] = [Hello, Gab]; 
